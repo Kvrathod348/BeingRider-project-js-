5c55d4f4ca6710ca46760a1e006aa65f465268c4
MarcusJadhav <marcusjadhav@gmail.com>
2024-07-22 01:14:43 +0530

    functionality added to accessories

--- js/spiti-valley.js
+++ js/spiti-valley.js
@@ -40,4 +40,31 @@ function searchResult() {
             title.innerText = "Shimla"
             info.innerText = "Shimla  also known as Simla, the official name until 1972)[10] is the capital and the largest city of the northern Indian state of Himachal Pradesh. In 1864, Shimla was declared as the summer capital of British India. After independence, the city became the capital of East Punjab and was later made the capital city of Himachal Pradesh. It is the principal commercial, cultural and educational centre of the state."
     }
-} 
+}
+
+function changeImg(num) {
+    const mainImg = document.getElementById("mainImg")
+    const buttonName = document.getElementById("product-link")
+
+
+    if (num == 1) {
+        mainImg.src = "./../assets/spiti-valley/full-set.webp"
+        buttonName.innerHTML = '<a href="https://m.vffshop.top/products.aspx?cname=bike+riding+full+kit+price&cid=42&url=www.ilcascinone.com" class="img-name font-style" target="_blank" id="product-link">Full Set</a>'
+    }
+    else if (num == 2) {
+        mainImg.src = "./../assets/spiti-valley/helmet.jpg"
+        buttonName.innerHTML = '<a href="https://www.amazon.in/Steelbird-Certified-Motocross-Double-Helmet/dp/B0B77TC58M/ref=asc_df_B0B77TC58M/?tag=googleshopdes-21&linkCode=df0&hvadid=619450542151&hvpos=&hvnetw=g&hvrand=7051107289065676040&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9198250&hvtargid=pla-1748976179424&psc=1&mcid=9a710759b969367aaebff3e04c6433d0" class="img-name font-style" target="_blank" id="product-link">Helmet</a>'
+    }
+    else if (num ==3 ) {
+        mainImg.src = "./../assets/spiti-valley/gloves.jpg"
+        buttonName.innerHTML = '<a href="https://www.amazon.in/XTRIM-Protekt-Touchscreen-Compatible-Fingertips/dp/B0C1NZ46M8?ref_=Oct_d_otopr_d_5258042031_0&pd_rd_w=uonld&content-id=amzn1.sym.601891be-591c-4695-b226-dfc2707ab366&pf_rd_p=601891be-591c-4695-b226-dfc2707ab366&pf_rd_r=H0BZPN1X00PRH9P7TZ0P&pd_rd_wg=1Ovp2&pd_rd_r=1cc26ddc-a27d-4a5f-a918-63369e090f6b&pd_rd_i=B0C1NZ46M8&th=1" class="img-name font-style" target="_blank" id="product-link">Gloves</a>'
+    }
+    else if (num == 4) {
+        mainImg.src = "./../assets/spiti-valley/jacket.webp"
+        buttonName.innerHTML = '<a href="https://store.royalenfield.com/en/windfarer-v2-green-3xl-48-cm-green?utm_source=google&utm_medium=cpc&utm_campaign=&utm_agid=&utm_term=&gad_source=1&gclid=CjwKCAjw4_K0BhBsEiwAfVVZ_8Scw3Ok_hT3Rr8_Y3fvfxlKt_eJFdKv9J5nJ7nC7pT-hnARlmGJERoCwqoQAvD_BwE" class="img-name font-style" target="_blank" id="product-link">Jacket</a>'
+    }
+    else if (num == 5) {
+        mainImg.src = "./../assets/spiti-valley/shoes.jpg"
+        buttonName.innerHTML =  '<a href="https://www.amazon.in/Axor-Slipstream-Riding-Boot-Black/dp/B0B2NXK5VC/ref=sr_1_11?dib=eyJ2IjoiMSJ9.dnO1FSbqeAtfh2BD_7SVWnPLasAHdja1GSeG7328MuQ0PLymFzmmj7MYOpJYp0a70_NjoIXmVDeYUY-gQgewm2JLPxC8RwcabYoH8Ug-xQs0rFS7_8TY1I7s9qWbHNgTrMOcaaeLvHrLljSHndkFwJ5ylUkGmAVuzi3ImhyQVewZiTCOKE4vQ7GhXk-xgz_2iZAWq_sxmCZM3N-YcKTD0pg29vi_65lnH0azW4iBi4yoKcQU5XYQCbh-zUuLXpsbrNtXfEB_Ps3ZWKqJ3Mklp2UrRCn6P9XfpWZ4-RSuGIc.AiNmbdkVVf9f47MgPVCo5RTICmozTcSWEKq1Av0E2QU&dib_tag=se&keywords=Riding+Boots&qid=1721590500&sr=8-11" class="img-name font-style" target="_blank" id="product-link">Boots</a>'
+    }
+}
