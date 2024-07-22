eeebee72f52fb97f734a7505f7879ef27a9966b0
rupalikorade <rupalikorade1563@gmail.com>
2024-07-22 14:46:59 +0530

    search result

---
+++ js/kerala-bike-trip.js
@@ -0,0 +1,13 @@
+function searchResult() {
+    const inputOption = document.getElementById("input")
+
+    const resultImg = document.getElementById("content1-img")
+
+    const title = document.getElementById("heading1")
+
+    const info = document.getElementById("para1")
+
+
+    const option = inputOption.value
+
+}
