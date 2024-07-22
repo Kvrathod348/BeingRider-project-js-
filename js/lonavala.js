d4aa34846db64e47f8aea47912c46c067d11d91d
unknown <rajeshwarikalhapure.d@gmail.com>
2024-07-22 01:38:11 -0700

    css modify

---
+++ js/lonavala.js
@@ -0,0 +1,9 @@
+function changeImg(num) {
+    const mainImg = document.getElementById("main-img")
+    const accLink = document.getElementById("acc-link")
+
+    if (num == 1) {
+        mainImg.src = "./../assets/lonavala/tvs,png.webp"
+        accLink.innerHTML 
+    }
+}
